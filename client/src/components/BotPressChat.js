696fddf929d5d15f2baaa8cd71dfcc39c435af7c
ASRA <asrajamalashraf@gmail.com>
2025-06-30 12:38:56 +0530

    chat bot fix

--- client/src/components/BotPressChat.js
+++ client/src/components/BotPressChat.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+/*import { useEffect } from "react";
 
 const BotpressChat = () => {
   useEffect(() => {
@@ -21,4 +21,38 @@ const BotpressChat = () => {
   return null; 
 };
 
+export default BotpressChat;*/
+
+import { useEffect } from "react";
+
+const BotpressChat = () => {
+  useEffect(() => {
+    // Check if the script is already injected
+    if (window.botpressWebChat) return;
+
+    // Load inject.js
+    const injectScript = document.createElement("script");
+    injectScript.src = "https://cdn.botpress.cloud/webchat/v3.0/inject.js";
+    injectScript.defer = true;
+
+    injectScript.onload = () => {
+      // Once inject.js is loaded, load config script
+      const configScript = document.createElement("script");
+      configScript.src = "https://files.bpcontent.cloud/2025/06/30/04/20250630042421-R011RS1G.js";
+      configScript.defer = true;
+      document.body.appendChild(configScript);
+    };
+
+    document.body.appendChild(injectScript);
+
+    // Clean up if component unmounts
+    return () => {
+      if (injectScript) document.body.removeChild(injectScript);
+    };
+  }, []);
+
+  return null;
+};
+
 export default BotpressChat;
+
